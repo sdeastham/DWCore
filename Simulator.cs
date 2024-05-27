baa4da860c521043bed713274365021e184ced59
Sebastian D. Eastham <sdeastham@googlemail.com>
2024-05-27 12:35:42 +0100

    Working loading screen

---
+++ Simulator.cs
@@ -0,0 +1,19 @@
+﻿using MathNet.Numerics.Random;
+
+namespace DroxtalWolf;
+
+public class Simulator
+{
+    public Simulator()
+    {
+        
+    }
+    public static Random GetNextRandomNumberGenerator(Random masterRandomNumberGenerator, ICollection<int> seedsUsed)
+    {
+        int seed;
+        do { seed = masterRandomNumberGenerator.Next(); } while (seedsUsed.Contains(seed));
+        Random subRandomNumberGenerator = new SystemRandomSource(seed);
+        seedsUsed.Add(seed);
+        return subRandomNumberGenerator;
+    }
+}
